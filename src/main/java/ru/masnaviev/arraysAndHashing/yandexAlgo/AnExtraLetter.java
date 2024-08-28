37e5eb79eb6d88ad1902b306e97c1f539e0bf0c7
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-08-28 16:53:39 +0800

    some tasks

---
+++ src/main/java/ru/masnaviev/arraysAndHashing/yandexAlgo/AnExtraLetter.java
@@ -0,0 +1,70 @@
+package ru.masnaviev.arraysAndHashing.yandexAlgo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AnExtraLetter {
+
+    public static void main(String[] args) throws IOException {
+        // Подготовка чтения и вывода
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder outputBuilder = new StringBuilder();
+        ////////////////////////////////////////////////////////
+
+        String s = reader.readLine();
+        String t = reader.readLine();
+
+        ////////////////////////////////////////////////////////
+        // Измерение времени
+        long startTime = System.nanoTime();
+
+        // Измерение памяти
+        Runtime runtime = Runtime.getRuntime();
+        long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+
+        //TODO
+
+        outputBuilder.append(extraLetter(s, t));
+
+        //TODO
+
+        // Измерение памяти после выполнения кода
+        long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        long memoryUsed = memoryAfter - memoryBefore;
+
+        long endTime = System.nanoTime();
+        long durationNano = endTime - startTime;
+
+        // Преобразование в секунды и мегабайты
+        double durationSeconds = durationNano / 1_000_000_000.0; // наносекунды в секунды
+        double memoryUsedMB = memoryUsed;
+
+        // Вывод результатов
+        System.out.println("Результат :" + outputBuilder);
+        System.out.printf("Время выполнения: %.6f секунд%n", durationSeconds);
+        System.out.printf("Используемая память: %.6f байт%n", memoryUsedMB);
+    }
+
+    public static char extraLetter(String s, String t) {
+        int size = t.length();
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Map<Character, Integer> map = new HashMap<>(size);
+
+        for (char c : sArr) {
+            map.put( c, map.getOrDefault(c, 0) + 1);
+        }
+
+
+        for (char c : tArr) {
+            map.put( c, map.getOrDefault(c, 0) - 1);
+        }
+
+        return map.entrySet().stream().filter(entry -> entry.getValue() == -1).collect(Collectors.toList()).get(0).getKey();
+    }
+}
