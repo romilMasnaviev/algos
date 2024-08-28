dee99289b0fbdebcf0d554ee6dcef234045e8a18
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-08-28 15:15:48 +0800

    some tasks

---
+++ src/main/java/ru/masnaviev/arraysAndHashing/yandexAlgo/DegreeOfFour.java
@@ -0,0 +1,68 @@
+package ru.masnaviev.arraysAndHashing.yandexAlgo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DegreeOfFour {
+
+    public static void main(String[] args) throws IOException {
+        // Подготовка чтения и вывода
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder outputBuilder = new StringBuilder();
+        ////////////////////////////////////////////////////////
+
+        int num = Integer.parseInt(reader.readLine());
+
+        ////////////////////////////////////////////////////////
+        // Измерение времени
+        long startTime = System.nanoTime();
+
+        // Измерение памяти
+        Runtime runtime = Runtime.getRuntime();
+        long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+
+        //TODO
+
+        outputBuilder.append(isDegreeOfFour(num));
+
+        //TODO
+
+        // Измерение памяти после выполнения кода
+        long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        long memoryUsed = memoryAfter - memoryBefore;
+
+        long endTime = System.nanoTime();
+        long durationNano = endTime - startTime;
+
+        // Преобразование в секунды и мегабайты
+        double durationSeconds = durationNano / 1_000_000_000.0; // наносекунды в секунды
+        double memoryUsedMB = memoryUsed;
+
+        // Вывод результатов
+        System.out.println("Результат = " + outputBuilder);
+        System.out.printf("Время выполнения: %.6f секунд%n", durationSeconds);
+        System.out.printf("Используемая память: %.6f байт%n", memoryUsedMB);
+    }
+
+    public static String isDegreeOfFour(int num) {
+        int i = 0;
+        while (true) {
+            int k = powOfFour(i);
+            if (k == num) {
+                return "True";
+            } else if (k > num) {
+                return "False";
+            }
+            i++;
+        }
+    }
+
+    public static int powOfFour(int i) {
+        int sum = 1;
+        for (int j = 0; j < i; j++) {
+            sum *= 4;
+        }
+        return sum;
+    }
+}
