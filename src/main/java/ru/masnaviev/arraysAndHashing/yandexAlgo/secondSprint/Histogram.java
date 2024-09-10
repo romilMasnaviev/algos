80192b264ea036792b2692dfe9b06131e3ac2bbd
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2024-09-10 12:18:19 +0300

    some tasks

---
+++ src/main/java/ru/masnaviev/arraysAndHashing/yandexAlgo/secondSprint/Histogram.java
@@ -0,0 +1,65 @@
+package ru.masnaviev.arraysAndHashing.yandexAlgo.secondSprint;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Histogram {
+    public static void main(String[] args) throws IOException {
+        // Подготовка чтения и вывода
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder outputBuilder = new StringBuilder();
+        ////////////////////////////////////////////////////////
+
+        int num = Integer.parseInt(reader.readLine());
+
+        int[] arr = new int[num];
+        String line = reader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        System.out.println(Arrays.toString(arr));
+        ////////////////////////////////////////////////////////
+        // Измерение времени
+        long startTime = System.nanoTime();
+
+        // Измерение памяти
+        Runtime runtime = Runtime.getRuntime();
+        long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+
+        //TODO
+
+        outputBuilder.append((" "));
+
+        //TODO
+
+        // Измерение памяти после выполнения кода
+        long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        long memoryUsed = memoryAfter - memoryBefore;
+
+        long endTime = System.nanoTime();
+        long durationNano = endTime - startTime;
+
+        // Преобразование в секунды и мегабайты
+        double durationSeconds = durationNano / 1_000_000_000.0; // наносекунды в секунды
+        double memoryUsedMB = memoryUsed;
+
+        // Вывод результатов
+        System.out.println("Результат :" + outputBuilder);
+        System.out.printf("Время выполнения: %.6f секунд%n", durationSeconds);
+        System.out.printf("Используемая память: %.6f байт%n", memoryUsedMB);
+    }
+
+
+    public static int calculateSquare(int[] arr){
+        //TODO
+
+    }
+
+
+}
